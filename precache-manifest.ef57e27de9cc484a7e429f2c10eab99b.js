c4b07989ffbb21801800b91288ae9284d671539a
Kevin Brown <48394009+kbrowngithub@users.noreply.github.com>
2020-02-21 14:38:03 -0700

    Updates

---
+++ precache-manifest.ef57e27de9cc484a7e429f2c10eab99b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee0db3159776035da2918d3ff445e592",
+    "url": "/kbrowngithub/MemoryGame/index.html"
+  },
+  {
+    "revision": "15f8b0e8cb887c176e42",
+    "url": "/kbrowngithub/MemoryGame/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8f8ad166f205b2a3aacb",
+    "url": "/kbrowngithub/MemoryGame/static/js/2.da652c0e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/kbrowngithub/MemoryGame/static/js/2.da652c0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15f8b0e8cb887c176e42",
+    "url": "/kbrowngithub/MemoryGame/static/js/main.5ae3d95a.chunk.js"
+  },
+  {
+    "revision": "8465fe27d7f9d043414f",
+    "url": "/kbrowngithub/MemoryGame/static/js/runtime-main.4f45ccfc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kbrowngithub/MemoryGame/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/kbrowngithub/MemoryGame/precache-manifest.ef57e27de9cc484a7e429f2c10eab99b.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/kbrowngithub/MemoryGame/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
